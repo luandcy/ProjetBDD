622fdc3add7a7a243d906972c5a1290497e3dd0f
NathanaelRR <nathanael.r.r@gmail.com>
2023-01-05 17:46:47 +0100

    Modifications de Nathanaël
    Ajout de données dans la base "projet_bdd" et un répertoire d'images pour les artistes.

--- projet_bdd.sql
+++ projet_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 05 jan. 2023 à 01:50
+-- Généré le : jeu. 05 jan. 2023 à 17:39
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.6
 
@@ -37,6 +37,16 @@ CREATE TABLE `artistes` (
   `IdGenre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `artistes`
+--
+
+INSERT INTO `artistes` (`IdArtiste`, `NomArtiste`, `TexteArtiste`, `NoteArtiste`, `DatePublicationArtiste`, `IdUser`, `IdGenre`) VALUES
+(1, 'Michael Jackson', 'Reconnu comme l’artiste le plus titré de tous les temps, il est une figure principale de l\'histoire de l\'industrie du spectacle et l\'une des icônes culturelles occidentales majeures du xxe siècle.', 4, '2023-01-05', 23, 1),
+(2, 'Travis Scott', 'Un rappeur, chanteur, producteur, musicien, acteur, mannequin et réalisateur américain. Il a aussi collaboré avec beaucoup de grandes marques. ', 3, '2023-01-05', 22, 5),
+(3, 'Bob Marley', 'Il rencontre de son vivant un succès mondial, et reste à ce jour le musicien le plus connu du reggae, tout en étant considéré comme celui qui a permis à la musique jamaïcaine et au mouvement rastafari de connaître une audience planétaire.', 3, '2023-01-05', 21, 6),
+(4, 'Rihanna', 'Rihanna est l\'artiste qui compte le plus grand nombre de certifications de singles. Elle est aussi la première artiste au monde à franchir le cap de 100 millions de ventes numériques aux États-Unis2.', 4, '2023-01-05', 24, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +62,15 @@ CREATE TABLE `avis` (
   `IdArtiste` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`IdAvis`, `TexteAvis`, `DateAvis`, `NoteAvis`, `IdUtilisateur`, `IdArtiste`) VALUES
+(1, 'Certe pour les fans c’est dur d’accepter que son idole soit parti .. j’aimerais y croire aussi mais malheureusement ce n’est pas le cas il est bien parti. ', '2023-01-05', 2, 22, 1),
+(2, 'Her music always makes me feel good! ', '2023-01-05', 4, 23, 4),
+(3, 'Kendrick’s verse is mad underrated !!! ', '2023-01-05', 3, 21, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -62,6 +81,18 @@ CREATE TABLE `genres` (
   `IdGenre` int(11) NOT NULL,
   `NomGenre` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `genres`
+--
+
+INSERT INTO `genres` (`IdGenre`, `NomGenre`) VALUES
+(1, 'POP'),
+(2, 'R and B'),
+(3, 'ROCK and ROLL'),
+(4, 'JAZZ'),
+(5, 'RAP'),
+(6, 'REGGAE');
 
 -- --------------------------------------------------------
 
@@ -74,6 +105,16 @@ CREATE TABLE `images` (
   `NomImage` varchar(60) NOT NULL,
   `IdArtiste` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `images`
+--
+
+INSERT INTO `images` (`IdImage`, `NomImage`, `IdArtiste`) VALUES
+(1, 'BobMarley', 3),
+(2, 'MichaelJackson', 1),
+(3, 'Rihanna', 4),
+(4, 'TravisScott', 2);
 
 -- --------------------------------------------------------
 
@@ -89,6 +130,16 @@ CREATE TABLE `utilisateurs` (
   `Avatar` varchar(50) NOT NULL,
   `Administrateur` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`IdUser`, `Pseudo`, `Password`, `Mail`, `Avatar`, `Administrateur`) VALUES
+(21, 'RNath', 'Toto_mdp', 'RN@gmail.fr', '', 0),
+(22, 'HU', 'Baap', 'HU.R@gmail.fr', '', 0),
+(23, 'Nathanael', 'mdp_nth', 'nath.Ra@gmail.fr', '', 1),
+(24, 'Luan', 'mdp_luan', 'luan.De@gmail.fr', '', 1);
 
 --
 -- Index pour les tables déchargées
@@ -137,31 +188,31 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `artistes`
 --
 ALTER TABLE `artistes`
-  MODIFY `IdArtiste` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdArtiste` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `avis`
 --
 ALTER TABLE `avis`
-  MODIFY `IdAvis` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdAvis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `IdGenre` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdGenre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `images`
 --
 ALTER TABLE `images`
-  MODIFY `IdImage` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdImage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Contraintes pour les tables déchargées
